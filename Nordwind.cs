057325f51d355ba82e25cb589e221da441f465b0
egurnova <egurnovaelena56@gmail.com>
2023-11-23 12:24:25 +0500

    Добавьте файлы проекта.

---
+++ Nordwind.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+
+
+namespace EFWithLinq
+{
+    internal class Nordwind :DbContext
+    {
+        public DbSet<Category> Categories => Set<Category>();
+        public DbSet<Product> Products => Set<Product>();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-KJ07M75\SQLEXPRESS;
+                                            Database=Northwind;
+                                            Trusted_Connection=True;
+                                            MultipleActiveResultSets=True; 
+                                            trustServerCertificate=True;
+                                            ");
+        }
+
+        
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace EFWithLinq
+{
+    internal class Program
+    {
+
+        static void FilterAndSort()
+        {
+            using (var db = new Nordwind())
+            {
+                var query = db.Products
+                    .Where(product => product.UnitPrice < 10M)
+                    .OrderByDescending(product => product.UnitPrice);
+
+                Console.WriteLine("товары дешевле 10$");
+                foreach (var product in query)
+                {
+                    Console.WriteLine($"{product.ProductID}: {product.ProductName}" +
+                        $" {product.UnitPrice:$#,##0.00}");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            FilterAndSort();
+
+            
+
+        }
+
+
+
+    }
+}
